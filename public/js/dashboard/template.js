85bf134c5443efd15d4dd4e04db6a619a99647f3
Jesse Boyer <aniyishay@gmail.com>
2013-07-02 21:56:35 -0400

    Complete

--- public/js/dashboard/template.js
+++ public/js/dashboard/template.js
@@ -32,8 +32,10 @@ var Template = function() {
         var output = '';
         output += '<div id="note_'+ obj.note_id +'">';
         output += '<span><a class="note_toggle" data-id="'+obj.note_id+'" id="note_title_'+ obj.note_id +'" href="#">' + obj.title + '</a></span> ';
-        output += '<a class="note_update_display" data-id="' + obj.note_id + '" href="#">Edit</a>';
+        output += '<span class="options">';
+        output += '<a class="note_update_display" data-id="' + obj.note_id + '" href="#"><i class="icon-pencil"></i></a>';
         output += '<a data-id="'+ obj.note_id +'" class="note_delete" href="api/delete_note"><i class="icon-remove"></i></a>';
+        output += '</span>';
         output += '<div class="note_edit_container" id="note_edit_container_'+ obj.note_id + '"></div>';
         output += '<div class="hide" id="note_content_'+ obj.note_id +'">'+obj.content+'</div>';
         output += '</div>'; 
@@ -44,12 +46,15 @@ var Template = function() {
     
     this.note_edit = function(note_id) {
         var output = '';
-        output += '<form method="post" class="note_edit_form" action="api/update_note">';
-        output += '<input class="title" type="text" name="title" />';
-        output += '<input class="note_id" type="hidden" name="note_id" value="'+note_id+'" />';
-        output += '<textarea class="content" name="content"></textarea>';
-        output += '<input type="submit" value="Save" />';
-        output += '<input type="submit" class="note_edit_cancel" value="Cancel" />';
+        output += '<form method="post" class="note_edit_form form-horizontal" action="api/update_note">';
+        output += '<input class="note_id" type="hidden" name="note_id" value="'+note_id+'" />';        
+        output += '<div class="input-append">';
+        output += '<input tabindex="1" type="text" class="title" name="title" placeholder="Note Title" />';
+        output += '<input tabindex="3" type="submit" class="btn btn-success" value="Update" />';
+        output += '<input type="submit" class="note_edit_cancel btn" value="Cancel" />';
+        output += '</div>';
+        output += '<div class="clearfix"></div>';
+        output += '<textarea tabindex="2" class="content" name="content"></textarea>';
         output += '</form>';
         return output;
     };
